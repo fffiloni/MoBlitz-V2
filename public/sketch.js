97cfbb15cba16680b94a10e26f84609cfb05bd1a
fffiloni <sylvain.fillion15@gmail.com>
2019-11-25 02:28:00 +0100

    added gif example

--- public/sketch.js
+++ public/sketch.js
@@ -77,9 +77,9 @@ function preload() {
     '');
   wlcm.parent('console');
 
-  // socket = io.connect('https://magmanin.nanomenta.com/');
-  // socket = io.connect('http://localhost:4000');
-	socket = io.connect('https://mb-duo.herokuapp.com/');
+  // socket = io.connect('https://magmanim.nanomenta.com/');
+  socket = io.connect('http://localhost:4000');
+	// socket = io.connect('https://mb-duo.herokuapp.com/');
 	scktClass.initializeDB();
 
 } //END PRELOAD
